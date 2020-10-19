aac6f2ac3c7e1eae344552fc0c947901291dcdc2
amitg1275 <amitg1275@gmail.com>
2020-10-19 09:57:25 +0000

    procedure1

--- core_node/router/routes/myNode.js
+++ core_node/router/routes/myNode.js
@@ -3,104 +3,104 @@
 "use strict";
 var express = require("express");
 
-module.exports = function() {
+module.exports = function () {
 	var app = express.Router();
 
 	//Hello Router
 	app.get("/", (req, res) => {
 		return res.type("text/plain").status(200).send("Hello World Node.js");
 	});
-//Simple Database Select - In-line Callbacks
-//Example1 handler
-app.get("/example1", (req, res) => {
-	let client = req.db;
-	client.prepare(
-		`SELECT "PurchaseOrderId" 
+	//Simple Database Select - In-line Callbacks
+	//Example1 handler
+	app.get("/example1", (req, res) => {
+		let client = req.db;
+		client.prepare(
+			`SELECT "PurchaseOrderId" 
 		                   FROM "PO.HeaderView"`,
-		(err, statement) => {
-			if (err) {
-				return res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
-			}
-			statement.exec([],
-				(err, results) => {
-					if (err) {
-						return res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
-					} else {
-						var result = JSON.stringify({
-							Objects: results
-						});
-						return res.type("application/json").status(200).send(result);
-					}
-				});
-			return null;
-		});
-	return null;
-});	
-
-//Simple Database Update - In-line Callbacks
-//Example1 handler
-app.get("/example3", (req, res) => {
-//var body = $.request.body.asString();
-//var oData = JSON.parse(body)
-  var oData = {
-    "currency": "EUR",
-    "po": "300000000"
-  };
-	let client = req.db;
-	client.prepare(
-          'UPDATE "PO.Header" SET CURRENCY = ? WHERE PURCHASEORDERID = ?',
-		(err, statement) => {
-			if (err) {
-				return res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
-			}
-			statement.exec([oData.currency,oData.po],
-				(err, results) => {
-					if (err) {
-						return res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
-					} else {
-//						client.commit(),
-						var result = JSON.stringify({
-							Objects: results
-						});
-						return res.type("application/json").status(200).send(result);
-					}
-				});
-			return null;
-		});
-	return null;
-});	
-
-var async = require("async");
-//Simple Database Select Via Client Wrapper/Middelware - Async Waterfall
-app.get("/example2", (req, res) => {
-	let client = req.db;
-	async.waterfall([
-		function prepare(callback) {
-			client.prepare(`SELECT SESSION_USER, CURRENT_SCHEMA
-											FROM "DUMMY"`,
-				(err, statement) => {
-					callback(null, err, statement);
-				});
-		},
-
-		function execute(err, statement, callback) {
-			statement.exec([], (execErr, results) => {
-				callback(null, execErr, results);
+			(err, statement) => {
+				if (err) {
+					return res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+				}
+				statement.exec([],
+					(err, results) => {
+						if (err) {
+							return res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+						} else {
+							var result = JSON.stringify({
+								Objects: results
+							});
+							return res.type("application/json").status(200).send(result);
+						}
+					});
+				return null;
 			});
-		},
-		function response(err, results, callback) {
-			if (err) {
-				res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
-			} else {
-				var result = JSON.stringify({
-					Objects: results
+		return null;
+	});
+
+	//Simple Database Update - In-line Callbacks
+	//Example1 handler
+	app.get("/example3", (req, res) => {
+		//var body = $.request.body.asString();
+		//var oData = JSON.parse(body)
+		var oData = {
+			"currency": "EUR",
+			"po": "300000000"
+		};
+		let client = req.db;
+		client.prepare(
+			'UPDATE "PO.Header" SET CURRENCY = ? WHERE PURCHASEORDERID = ?',
+			(err, statement) => {
+				if (err) {
+					return res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+				}
+				statement.exec([oData.currency, oData.po],
+					(err, results) => {
+						if (err) {
+							return res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+						} else {
+							//						client.commit(),
+							var result = JSON.stringify({
+								Objects: results
+							});
+							return res.type("application/json").status(200).send(result);
+						}
+					});
+				return null;
+			});
+		return null;
+	});
+
+	var async = require("async");
+	//Simple Database Select Via Client Wrapper/Middelware - Async Waterfall
+	app.get("/example2", (req, res) => {
+		let client = req.db;
+		async.waterfall([
+			function prepare(callback) {
+				client.prepare(`SELECT SESSION_USER, CURRENT_SCHEMA
+											FROM "DUMMY"`,
+					(err, statement) => {
+						callback(null, err, statement);
+					});
+			},
+
+			function execute(err, statement, callback) {
+				statement.exec([], (execErr, results) => {
+					callback(null, execErr, results);
 				});
-				res.type("application/json").status(200).send(result);
+			},
+			function response(err, results, callback) {
+				if (err) {
+					res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+				} else {
+					var result = JSON.stringify({
+						Objects: results
+					});
+					res.type("application/json").status(200).send(result);
+				}
+				return callback();
 			}
-			return callback();
-		}
-	]);
-});	
+		]);
+	});
 
 	//Simple Database Select Via Client Wrapper/Middelware - Promises
 	app.get("/promises", (req, res) => {
@@ -124,22 +124,22 @@ app.get("/example2", (req, res) => {
 				return res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
 			});
 	});
-	
-//	Simple Database Select Via Client Wrapper/Middelware - Await
+
+	//	Simple Database Select Via Client Wrapper/Middelware - Await
 	app.get("/await", async(req, res) => {
-  var oData = {
-    "currency": "LJP",
-    "po": "300000000"
-  };		
-			const dbClass = require(global.__base + "utils/dbPromises");
-			let db = new dbClass(req.db);  
+		var oData = {
+			"currency": "LJP",
+			"po": "300000000"
+		};
+		const dbClass = require(global.__base + "utils/dbPromises");
+		let db = new dbClass(req.db);
 		try {
 			const statement = await db.preparePromisified('UPDATE "PO.Header" SET CURRENCY = ? WHERE PURCHASEORDERID = ?');
-			const results = await db.statementExecPromisified(statement, ["AAA",oData.po]);
-			
-			const statement1 = await db.preparePromisified('UPDATE "PO.Header123" SET CURRENCY = ? WHERE PURCHASEORDERID = ?');
-			const results1 = await db.statementExecPromisified(statement1, [oData.currency,oData.po]);			
-			db.client.setAutoCommit(true);	
+			const results = await db.statementExecPromisified(statement, ["AAA", oData.po]);
+
+			const statement1 = await db.preparePromisified('UPDATE "PO.Header1" SET CURRENCY = ? WHERE PURCHASEORDERID = ?');
+			const results1 = await db.statementExecPromisified(statement1, [oData.currency, oData.po]);
+			db.client.setAutoCommit(true);
 			let result = JSON.stringify({
 				Objects: results
 			});
@@ -151,5 +151,27 @@ app.get("/example2", (req, res) => {
 		}
 	});
 	
+	//Simple Database Call Stored Procedure
+	app.get("/procedures", (req, res) => {
+		var client = req.db;
+		var hdbext = require("@sap/hdbext");
+		//(client, Schema, Procedure, callback)
+		hdbext.loadProcedure(client, null, "getPOItemsSQL", (err, sp) => {
+			if (err) {
+				res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+				return;
+			}
+			//(Input Parameters, callback(errors, Output Scalar Parameters, [Output Table Parameters])
+			sp({}, (err, parameters, results) => {
+				if (err) {
+					res.type("text/plain").status(500).send(`ERROR: ${err.toString()}`);
+				}
+				var result = JSON.stringify({
+					EX_TOP_3_EMP_PO_COMBINED_CNT: results
+				});
+				res.type("application/json").status(200).send(result);
+			});
+		});
+	});
 	return app;
 };
